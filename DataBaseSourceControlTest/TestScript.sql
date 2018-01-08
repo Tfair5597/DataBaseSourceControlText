abb0c2eb7af4c7c5a53b5471d51bb2779671e586
Jasper-Lawliet <Tfair5597@users.noreply.github.com>
2018-01-08 14:12:41 -0800

    Added comments describing issues and branches on github.

--- DataBaseSourceControlTest/TestScript.sql
+++ DataBaseSourceControlTest/TestScript.sql
@@ -9,3 +9,11 @@ CREATE TABLE Table1
 (
 /* columns here */
 )
+/* there's an issue with github in which working in a team can be a problem if multiple people work on the same "branch"
+branches are variations of the program i think? by separating a repo into multiple branches we can have each team member
+work on one part of a project, push the changes independently and eventually merge everything back into to the master
+branch.
+we can also designate, or ask for certain features, bugs, or components of a program via issues on github.
+we can label these issues, assign them to team members or ask for help on it. this is great especially if the project
+is open source. team members can comment on issues as well.
+*/
